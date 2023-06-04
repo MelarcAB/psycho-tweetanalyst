5c39d3aca49012118b46295b97a772111357acec
MelarcAB <melarc.ab@gmail.com>
2023-06-04 18:34:18 +0200

    try/catch added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,44 +12,50 @@ class ApiController extends Controller
 
     function tweets($username)
     {
-        $tweets = [];
-        $lastTweetId = null;
-        $params = [
-            'screen_name' => $username,
-            'count' => 15,
-            'format' => 'array',
-            'tweet_mode' => 'extended'
-        ];
-
-        $tweets_msg = [];
-
-
-
-        $batch = Twitter::getUserTimeline($params);
-
-        foreach ($batch as $tweet) {
-            $tweets[] = [
-                'username' => $tweet['user']['screen_name'],
-                'tweet_id' => $tweet['id_str'],
-                'date' => $tweet['created_at'],
-                'message' => $tweet['full_text'],
-                'reply_to' => $tweet['in_reply_to_screen_name'],
-                'reply_to_id' => $tweet['in_reply_to_status_id_str'],
-                'image' => $tweet['user']['profile_image_url_https']
+        try {
+            $tweets = [];
+            $lastTweetId = null;
+            $params = [
+                'screen_name' => $username,
+                'count' => 15,
+                'format' => 'array',
+                'tweet_mode' => 'extended'
             ];
-            $tweets_msg[] = $tweet['full_text'] . $tweet['created_at'];
+
+            $tweets_msg = [];
+
+
+
+            $batch = Twitter::getUserTimeline($params);
+
+            foreach ($batch as $tweet) {
+                $tweets[] = [
+                    'username' => $tweet['user']['screen_name'],
+                    'tweet_id' => $tweet['id_str'],
+                    'date' => $tweet['created_at'],
+                    'message' => $tweet['full_text'],
+                    'reply_to' => $tweet['in_reply_to_screen_name'],
+                    'reply_to_id' => $tweet['in_reply_to_status_id_str'],
+                    'image' => $tweet['user']['profile_image_url_https']
+                ];
+                $tweets_msg[] = $tweet['full_text'] . $tweet['created_at'];
+            }
+
+            $response = [
+                "tweets" => $tweets,
+                "tweets_msg" => $tweets_msg,
+                "batch" => $batch,
+                "params" => $params,
+                "username" => $username,
+                "lastTweetId" => $lastTweetId,
+            ];
+
+            return json_encode($response);
+        } catch (\Exception $e) {
+            return json_encode([
+                "error" => $e->getMessage()
+            ]);
         }
-
-        $response = [
-            "tweets" => $tweets,
-            "tweets_msg" => $tweets_msg,
-            "batch" => $batch,
-            "params" => $params,
-            "username" => $username,
-            "lastTweetId" => $lastTweetId,
-        ];
-
-        return json_encode($response);
     }
 
 
